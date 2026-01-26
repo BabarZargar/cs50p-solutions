8854369eeb29bef6a7585e655c45c52c55b601bd
BabarZargar <babarzargar95@gmail.com>
2026-01-26 20:53:02 +0530

    Delete week 7/twitter.py

--- week 7/twitter.py
+++
@@ -1,7 +0,0 @@
-import re
-
-url = input("URL: ").strip()
-
-if matches := re.search(r"^(?:https?://)?(?:www\.)?twitter\.com/([a-z0-9_]+)h", url, re.IGNORECASE):
-    print(f"Username:", matches.group(1))
-
